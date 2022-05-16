3c1dcab3bf497131ab26de35dc5ff75fb3650f21
moveside <jekker77@gmail.com>
2022-05-17 00:41:56 +0900

    connect error

--- src/main/java/moveside/web/IndexController.java
+++ src/main/java/moveside/web/IndexController.java
@@ -1,6 +1,7 @@
 package moveside.web;
 
 
+import lombok.RequiredArgsConstructor;
 import moveside.domain.Entity.ESL;
 import moveside.domain.Entity.Store;
 import moveside.domain.Repository.EslRepository;
@@ -8,16 +9,15 @@ import moveside.service.MenuService;
 import moveside.service.PostsService;
 import moveside.service.StoreService;
 import moveside.web.dto.PostsResponseDto;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -27,7 +27,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class IndexController {
 
     private final PostsService postsService;
@@ -58,9 +58,6 @@ public class IndexController {
                 model.addAttribute("test","ttt");
             }
         }
-
-
-
         return "login";
     }
 
