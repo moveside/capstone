ba9ad9f51fd03a41b7526df68a36ba17f5acabda
JinSung Kim <jinsungkim@JinSungui-MacBookAir.local>
2022-05-18 00:31:24 +0900

    first front

---
+++ capstone/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import React from "react";
+import NavBar from "./components/NavBar";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Login } from "./pages/Login";
+import { Contact } from "./pages/Contact";
+import { TestLogin } from "./Login/TestLogin";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+          {/*<div>
+            <TestLogin />
+            <Switch>
+              <Route exact path="/home" component={Home} />
+            </Switch>
+          </div>*/}
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ capstone/src/Login/TestLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export const TestLogin=()=>{
+    return(
+        <div>
+            <Link to="/home">
+                <button>Home</button>
+            </Link>
+        </div>
+    );
+};
