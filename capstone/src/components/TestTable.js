d8bb9a9c3b42bcc16333c18e873ab02f5137b699
JinSungKim <winnertree@naver.com>
2022-05-19 13:49:58 +0900

    mui table

--- capstone/src/components/TestTable.js
+++ capstone/src/components/TestTable.js
@@ -1,9 +1,36 @@
 import axios from "axios";
 import React, {uesEffect, useEffect, useState} from "react";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+}));
+  
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+}));
 
 const TestTable=()=>{
-    const [product, setProduct] = useState([])
+    const [product, setProduct] = useState([]);
 
     const getProductData = async() =>{
         try{
@@ -19,17 +46,46 @@ const TestTable=()=>{
         getProductData();
     },[]);
     return(
-        <div className="TestTable">
-            <h1>plz plz!</h1>
-            {product.map((item)=>{
-                return(
-                <p>
-                    {item.name} - {item.price}
-                </p>
-                );
-                })}
-        </div>
+        
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                    <StyledTableCell>id</StyledTableCell>
+                    <StyledTableCell align="right">name</StyledTableCell>
+                    <StyledTableCell align="right">price</StyledTableCell>
+                    <StyledTableCell align="right">info</StyledTableCell>
+                    <StyledTableCell align="right">store</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {product
+                    .map((item) => {
+                        return (
+                        <StyledTableRow key={item.id}>
+                            <StyledTableCell component="th" scope="row">
+                            {item.id}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">
+                            {item.name}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">
+                            {item.price}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">
+                            {item.info}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">
+                            {item.store}
+                            </StyledTableCell>
+                        </StyledTableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
+    
 };
 
 export default TestTable
