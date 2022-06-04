08fbf652c3be5a5f4a252b1132e1fea2198d2d96
김진성 <winnertree@naver.com>
2022-06-04 13:16:40 +0900

    진짜 최종안

--- FrontEnd/src/pages/ESL.js
+++ FrontEnd/src/pages/ESL.js
@@ -1,14 +1,11 @@
 import React from "react";
 import CallApi from "../components/CallApi";
-import Footer2 from "./Footer";
+
 export const ESL = () => {
     return (
         <>
             <div>
                 <CallApi />
-            </div>
-            <div>
-                <Footer2></Footer2>
             </div>
         </>
     );
